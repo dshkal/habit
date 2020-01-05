3304edeb5fa1e284ba5f5d0da5224ad4bba19f6e
Denis <datzyuk.den@gmail.com>
2020-01-05 21:16:55 +0200

    hardcoded log out button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit/bloc/authentication_bloc/authentication_bloc.dart';
+import 'package:habit/bloc/authentication_bloc/authentication_event.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(child: Text('Home page'),),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  BlocProvider.of<AuthenticationBloc>(context)..add(LoggedOut());
+                },
+                child: Text(
+                  'Log out'
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
